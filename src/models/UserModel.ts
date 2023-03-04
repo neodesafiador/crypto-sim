a87072cf1ca7c09f937c4305517368522a8e32a8
HoonKa <younghoo.kang@smail.astate.edu>
2023-03-04 18:55:32 +0900

    chaging code to chris's code

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -3,36 +3,72 @@ import { User } from '../entities/User';
 
 const userRepository = AppDataSource.getRepository(User);
 
+async function allUserData(): Promise<User[]> {
+  const allUsers = await userRepository.find();
+
+  return allUsers;
+} // getting back all of the data. makesure to take out the data for the project.
+
 async function addUser(email: string, passwordHash: string): Promise<User> {
   // Create the new user object
   let newUser = new User();
   newUser.email = email;
   newUser.passwordHash = passwordHash;
 
+  // Then save it to the database
+  // NOTES: We reassign to `newUser` so we can access
+  // NOTES: the fields the database autogenerates (the id & default columns)
   newUser = await userRepository.save(newUser);
 
   return newUser;
 }
 
+async function getAllUsers(): Promise<User[]> {
+  return userRepository.find();
+}
+
+async function getAllUnverifiedUsers(): Promise<User[]> {
+  return userRepository.find({
+    select: { email: true, userId: true },
+    where: { verifiedEmail: false },
+  });
+}
+
 async function getUserByEmail(email: string): Promise<User | null> {
   const user = await userRepository.findOne({ where: { email } });
+
   return user;
 }
 
-// async function getUserById(userId: string): Promise<User[] | null> {
-//   // const user = await userRepository.findOne({ where: { userId } });
-//   // return user;
+async function getUserById(userId: string): Promise<User[] | null> {
+  if (!userId) {
+    return null;
+  }
+  const user = await userRepository
+    .createQueryBuilder('user')
+    .where({ userId })
+    .select(['user.email', 'user.profileViews', 'user.joined0n', 'user.userId'])
+    .getMany();
 
-//   if (!userId) {
-//     return null;
-//   }
-//   const user = await userRepository
-//     .createQueryBuilder('user')
-//     .where({ userId })
-//     .select(['user.email', 'user.userId'])
-//     .getMany();
+  return user;
+}
 
-//   return user;
-// }
+async function getViralUsers(): Promise<User[]> {
+  const viralUsers = await userRepository
+    .createQueryBuilder('user')
+    .where('profileViews >= :viralAmount', { viralAmount: 1000 })
+    .select(['user.email', 'user.profileViews', 'user.userId'])
+    .getMany();
 
-export { addUser, getUserByEmail };
+  return viralUsers;
+} // User[] : returning array of the user.
+
+export {
+  allUserData,
+  addUser,
+  getUserByEmail,
+  getUserById,
+  getAllUsers,
+  getAllUnverifiedUsers,
+  getViralUsers,
+};
