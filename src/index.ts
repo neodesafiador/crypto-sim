03d66fefc81c95423883583220d581524cdd7252
HoonKa <younghoo.kang@smail.astate.edu>
2023-03-15 18:34:35 +0900

    crypto cusrrencies api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './config'; // Load environment variables
 import 'express-async-errors'; // Enable default error handling for async errors
 import express, { Express } from 'express';
-import { registerUser, logIn } from './controllers/UserController';
+import { registerUser, logIn, printCryptoCurrencies } from './controllers/UserController';
 
 const app: Express = express();
 app.use(express.json());
@@ -10,6 +10,7 @@ const { PORT } = process.env;
 
 app.post('/api/users', registerUser); // Create an account
 app.post('/api/login', logIn); // Log in to an account
+app.get('/api/printCryptoCurrencies', printCryptoCurrencies);
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
